fae391ce5598442f4f75d11fc4827995abdd5c98
Victory <git@dfhu.org>
2015-11-10 19:07:09 -0500

    get input value

--- src/org/dfhu/inputrussian/InputrussianUI.java
+++ src/org/dfhu/inputrussian/InputrussianUI.java
@@ -2,13 +2,20 @@ package org.dfhu.inputrussian;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.dfhu.inputrussian.rudb.TestDb;
+import org.dfhu.inputrussian.rudb.TestRow;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -23,17 +30,29 @@ public class InputrussianUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
+		
 		final VerticalLayout layout = new VerticalLayout();
 		layout.setMargin(true);
 		setContent(layout);
-
+		
+		FormLayout form = new FormLayout();
+		
+		TextField targetWord = new TextField("targetWord");
+		
+		targetWord.setCaption("Target word");
+		targetWord.setInputPrompt("A russian word");
+		form.addComponent(targetWord);
 		Button button = new Button("Give me a click");
 		button.addClickListener(new Button.ClickListener() {
 			public void buttonClick(ClickEvent event) {
 				layout.addComponent(new Label("Thank you for clicking"));
+				TestRow testRow = (TestRow) TestDb.getInstance().first("1 = 1");
+				Notification.show(targetWord.getValue());
 			}
 		});
-		layout.addComponent(button);
+		form.addComponent(button);
+		
+		layout.addComponent(form);
 	}
 
 }
